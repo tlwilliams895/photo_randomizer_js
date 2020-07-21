9459fda5af33177dac3ed23a4cb4758c1b1196dd
Tyisha Williams <tlwilliams895@gmail.com>
2020-07-21 17:39:29 -0400

    update to files

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 // Starter code provide by Randy via Demo
+let pageIndex = 1;
+
 let options = {
     enableHighAccuracy: true,
     maximumAge: 0
@@ -17,44 +19,44 @@ function constructImageURL(photoObj) {
         "/" + photoObj.id + "_" + photoObj.secret + ".jpg";
 };
 
-const photoURL = constructImageURL(response.photos.photo[0]);
+// const imageUrl = constructImageURL(response.photos.photo[0]);
 
 
 // Use fetch to send the request to Flickr
-function getFlickrPhotos(coordinates) {
-    console.log(`Latitude: ${coordinates.latitude}`);
-    console.log(`Longitude: ${coordinates.longitude}`);
+function retrievePhotos(coords) {
+    console.log(`Latitude: ${coords.latitude}`);
+    console.log(`Longitude: ${coords.longitude}`);
 
-    const flickrURL = "https://shrouded-mountain-15003.herokuapp.com/https://flickr.com/services/rest/?api_key=44b40b0700e3e2b4cac39a0d304f2007&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&latitude=" + coordinates.latitude + "&longitude=" + coordinates.longitude + "&text=hawaii"
+    const imagesURL = "https://shrouded-mountain-15003.herokuapp.com/https://flickr.com/services/rest/?api_key=44b40b0700e3e2b4cac39a0d304f2007&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=1&text=water& lat=" + coords.latitude + " & lon=" + coords.longitude + " & page=" + pageIndex
 
-    // fetch() synopsis:
-    // 1. fetch() will return a Promise (and you use Promise.prototype.then() on Promises).
-    fetch(flickrURL)
-        // 2. That Promise will provide a Response object to .then().
-        .then(function (response) {
-            // 3. That Response object has a method called .json().
-            // 4. .json() will return a Promise.
-            return response.json(); //hydrate
-        })
-        // 5. That Promise will provide the hydrated data to .then().
-        .then(displayImages);
+
+    let grabPromise = fetch(imagesURL)
+    let dataPromise = grabPromise.then(function (response) {
+        return response.json()
+    });
+
+    dataPromise.then(function (data) {
+        let pictures = constructImageURL(data.photos.photo[0]);
+        let imageContainer = document.querySelector(".images");
+        imageContainer.src = pictures
+    });
+
 };
 
 // Process the response data into an object
-function useCurrentLocation(position) {
-    console.log(position);
-    getFlickrPhotos(position.coordinates);
+function useRealLocation(pos) {
+    // console.log(pos);
+    retrievePhotos(pos.coords);
+    let imgBtn = document.getElementById("photoBtn");
+    imgBtn.addEventListener("click", function () {
+        retrievePhotos(pos.coords, pageIndex);
+        pageIndex++;
+    })
 };
-
-navigator.geolocation.getCurrentPosition(useCurrentLocation, useFallbackLocation, options);
 
 // Use the values in the response object to construct an image source URL
 function useFallbackLocation() {
-    getFlickrPhotos(fallbackLocation);
+    retrievePhotos(fallbackLocation);
 };
 
-// Display the first image on the page
-function displayImages(dataObj) {
-    console.log(dateObj);
-    console.log(constructImageURL(dataObj.photo.photo[0]));
-};
+navigator.geolocation.getCurrentPosition(useRealLocation, useFallbackLocation, options);
